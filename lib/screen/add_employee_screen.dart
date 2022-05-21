4e7fa3195a2dcaf8c2c139e414ba8e0a471ff30e
tianreformis <tianreformis@gmail.com>
2022-05-21 14:07:21 +0800

    add config

--- lib/screen/add_employee_screen.dart
+++ lib/screen/add_employee_screen.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: unused_field
+
 import 'package:flutter/material.dart';
 
 class AddEmployeeScreen extends StatefulWidget {
@@ -8,15 +10,48 @@ class AddEmployeeScreen extends StatefulWidget {
 }
 
 class _AddEmployeeScreenState extends State<AddEmployeeScreen> {
+  final TextEditingController _controller = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Add Employee'),
         centerTitle: true,
-        actions: [IconButton(onPressed: () {}, icon: Icon(Icons.save))],
+        actions: [IconButton(onPressed: () {}, icon: const Icon(Icons.save))],
       ),
-      body: Column(children: []),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(children: [
+          TextFormField(
+            controller: _controller,
+            keyboardType: TextInputType.name,
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'User name cannot be empty';
+              }
+              return null;
+            },
+            decoration: const InputDecoration(
+              border: OutlineInputBorder(),
+              label: Text('Username'),
+            ),
+          ),
+          TextFormField(
+            controller: _controller,
+            keyboardType: TextInputType.name,
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'User name cannot be empty';
+              }
+              return null;
+            },
+            decoration: const InputDecoration(
+              border: OutlineInputBorder(),
+              label: Text('Username'),
+            ),
+          )
+        ]),
+      ),
     );
   }
 }
--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -5,6 +5,18 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+        centerTitle: true,
+      ),
+      body: Column(children: []),
+      floatingActionButton: FloatingActionButton.extended(
+          onPressed: () {
+            Navigator.pushNamed(context, '/add_employee');
+          },
+          icon: const Icon(Icons.add),
+          label: const Text('Add Employee')),
+    );
   }
 }
