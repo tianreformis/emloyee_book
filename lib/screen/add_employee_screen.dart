d4eb99a2919b4703403cffce367c5423c05a9d3c
tianreformis <tianreformis@gmail.com>
2022-05-22 22:45:20 +0800

    edit bug on dateTime

--- lib/screen/add_employee_screen.dart
+++ lib/screen/add_employee_screen.dart
@@ -1,6 +1,7 @@
-// ignore_for_file: unused_field
+// ignore_for_file: unused_field, unused_local_variable
 
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 import '../widget/custom_text_form_field.dart';
 
@@ -16,6 +17,7 @@ class _AddEmployeeScreenState extends State<AddEmployeeScreen> {
   final TextEditingController _firstNameController = TextEditingController();
   final TextEditingController _lastNameController = TextEditingController();
   final TextEditingController _dateOfBirthcontroller = TextEditingController();
+  DateTime? _dateOfBirth;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -48,12 +50,51 @@ class _AddEmployeeScreenState extends State<AddEmployeeScreen> {
           const SizedBox(
             height: 8.0,
           ),
-          CostumTextFormField(
+          TextFormField(
             controller: _dateOfBirthcontroller,
-            txtLabel: 'Date Of Birth',
+            keyboardType: TextInputType.name,
+            decoration: const InputDecoration(
+              border: OutlineInputBorder(),
+              label: Text('Date of Birth'),
+            ),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Date of Birth Cannot Be Empty';
+              }
+              return null;
+            },
+            onTap: () => pickDateOfBirth(context),
           )
         ]),
       ),
     );
+  }
+
+  Future<void> pickDateOfBirth(BuildContext context) async {
+    final initialDate = DateTime.now();
+    final newDate = await showDatePicker(
+        context: context,
+        initialDate: _dateOfBirth ?? initialDate,
+        firstDate: DateTime(DateTime.now().year - 100),
+        lastDate: DateTime(DateTime.now().year + 1),
+        builder: (context, child) => Theme(
+              data: ThemeData().copyWith(
+                colorScheme: const ColorScheme.light(
+                  primary: Colors.pink,
+                  onPrimary: Colors.white,
+                  onSurface: Colors.black,
+                ),
+                dialogBackgroundColor: Colors.white,
+              ),
+              child: child ?? const Text(' '),
+            ));
+
+    if (newDate == null) {
+      return;
+    }
+    setState(() {
+      String dob = DateFormat('dd/MM/yyyy').format(newDate);
+      _dateOfBirthcontroller.text = dob;
+    });
   }
 }
